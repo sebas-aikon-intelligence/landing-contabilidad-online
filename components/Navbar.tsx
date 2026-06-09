48f4c9ee256c7b4cec19f0b3b886e5f9697c5af2
sebas-aikon-intelligence <sebas@aikonintelligence.com>
2026-06-09 12:41:06 -0500

    Update WhatsApp contact link in Navbar component

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,7 +4,7 @@ import { useState, useEffect } from 'react'
 import { Menu, X, Zap } from 'lucide-react'
 
 const WA_URL =
-  'https://api.whatsapp.com/send/?phone=573244614444&text=Hola.+Quiero+informaci%C3%B3n+de+los+planes&type=phone_number&app_absent=0'
+  'https://api.whatsapp.com/send/?phone=573024695956&text=Hola.+Quiero+informaci%C3%B3n+de+los+planes&type=phone_number&app_absent=0'
 
 const links = [
   { label: 'Características', href: '#features' },
